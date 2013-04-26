a8b3fb9ffdd79b2800e433a8f1054342c8858584
Julian Tatsch <tatsch@gmail.com>
2013-04-26 12:24:48 +0200

    fixed rotate convenience function

--- Java/src/PhoenixCommander.java
+++ Java/src/PhoenixCommander.java
@@ -269,7 +269,9 @@ public static BufferedImage rotate(BufferedImage image, double angle) {
     double sin = Math.abs(Math.sin(angle)), cos = Math.abs(Math.cos(angle));
     int w = image.getWidth(), h = image.getHeight();
     int neww = (int)Math.floor(w*cos+h*sin), newh = (int)Math.floor(h*cos+w*sin);
-    GraphicsConfiguration gc = getDefaultConfiguration();
+    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    GraphicsDevice gd = ge.getDefaultScreenDevice();
+    GraphicsConfiguration gc = gd.getDefaultConfiguration();
     BufferedImage result = gc.createCompatibleImage(neww, newh, Transparency.TRANSLUCENT);
     Graphics2D g = result.createGraphics();
     g.translate((neww-w)/2, (newh-h)/2);
